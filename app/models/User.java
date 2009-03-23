b09524b63d83b2fd2ec1ed4e826949e9acfff76a
arthur <arthur@agileway.(none)>
2009-03-23 19:18:24 +0800

    add virtual team model

--- app/models/User.java
+++ app/models/User.java
@@ -13,8 +13,48 @@ import play.db.jpa.JPAModel;
 @Table(name = "users")
 public class User extends JPAModel {
 
+    public String name;
+    public String email;
+    public String password;
+
+    public User() {
+    }
+
+    private User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static User create_user(String name, String email, String password) {
+
+        //TODO add validate password repeated
+        //TODO add password md5 encrypt
+        User user = new User(null, name, email, password);
+        user.save();
+        return user;
+    }
+
+    public static User udpate_user(User user) {
+        user.merge();
+        return user;
+    }
+
     /**
-     * 
+     *
      * @param userForm
      * @return
      */
@@ -36,45 +76,6 @@ public class User extends JPAModel {
         user.name = userForm.name;
         user.email = userForm.email;
         user.password = userForm.password;
-        return user;
-    }
-    public String name;
-    public String email;
-    public String password;
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    private User(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public static User create_user(String name, String email, String password) {
-
-        //TODO add validate password repeated
-        //TODO add password md5 encrypt
-        User user = new User(null, name, email, password);
-        user.save();
-        return user;
-    }
-
-    public static User udpate_user(User user) {
-        user.merge();
         return user;
     }
 }
