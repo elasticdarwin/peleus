34522ccc174100baa0d54d53cfa0361c6a81eee9
Darwin <zhelee.neu@gmail.com>
2009-05-18 22:50:17 +0800

    graphit.rb bug fix

--- graphit.rb
+++ graphit.rb
@@ -9,14 +9,14 @@ file_set = {}
 
 g.log.enum_for(:each).to_a.reverse.inject do |last, cur|
     diff = g.diff(last, cur)
-    total = total + diff.lines
+    total_stats = diff.stats[:total]
+    total = total + total_stats[:insertions] - total_stats[:deletions]
     diff_files = diff.stats[:files].sort { |a, b| a[0] <=> b[0] }
     
     diff_files.each do |file_name, stats|
         printf("%-60s %6d %6d\n", file_name, stats[:insertions], stats[:deletions])
     end
 
-    total_stats = diff.stats[:total]
     puts "From revision <#{diff.from}> to <#{diff.to}>"
     puts "Total line number is <#{total}>, with <#{total_stats[:insertions]}> insertions and <#{total_stats[:deletions]}> deletions in this interation."
 
