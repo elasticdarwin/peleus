9458500414fcca2cb0e1b52de532e7672982558c
zd <zd@zd-desktop.(none)>
2009-07-24 10:33:19 +0800

    整理selenium测试

--- test/unit/StateMachineTest.java
+++ test/unit/StateMachineTest.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package application;
+package unit;
 
 import models.ShareSession;
 import org.junit.Test;
